dd171fe7e95dd3c0060cea38ffca6d3e4a1ccdb7
vominhtam1704 <tam18042021@gmail.com>
2026-03-10 09:05:15 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/user-role-db';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/my-mongo';
 
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
